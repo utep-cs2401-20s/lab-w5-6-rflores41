dd0860632d2dc85a8d6c4fe58df1764ed6a170a3
Darks <rflores41@miners.utep.edu>
2020-02-24 14:48:16 -0700

    Worked on exhaustive search

--- java/src/SnakeGame.java
+++ java/src/SnakeGame.java
@@ -17,20 +17,37 @@ public class SnakeGame {
         this.headPosition[0] = x;
         this.headPosition[1] = y;
     }
-    //Methods for functions//////////////////////////////////////////////////////////////////////
-    public int[] findTailExhaustive(boolean [][] board){
-        for(int i =0; i < board.length; i++) {
-            for (int j = 0; j < board.length; j++) { //increment exhaustiveChecks
-                if (board[i][j] == true) {       //if part of the snake, look for neighbors, else go to next cell
 
-                }                              //either at head or tail if 1 neighbor, check with headPosition
-                //2 or more neighbors, move to next cell
-                //count every piece of the snake
-                //return tail(x,y)
+    //Methods for functions//////////////////////////////////////////////////////////////////////
+    public int[] findTailExhaustive(boolean [][] board) {
+        resetCounters();                                //reset counters before starting
+        int neighbors = 0;                              //counter for neighbors
+        int[] tailAndLength = new int[3];
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board.length; j++) {
+                exhastiveChecks ++;                  //increment exhaustiveChecks
+                if (board[i][j] == true) {           //if part of the snake
+                    if (i != headPosition[0] && j != headPosition[1]) {         //if not the head
+                        FindNeighbors(i, j, board);          //check for neighbors
+                        if (neighbors == 1) {                //if only 1 neighbor, mark as tail position
+                            tailAndLength[0] = i;
+                            tailAndLength[1] = j;
+                        }else if(neighbors >=2){
+                            
+                        }
+                    }
+                    //2 or more neighbors, move to next cell
+                    //count every piece of the snake
+                    //return tail(x,y)
+                }
             }
-        }
-        return  //return the tail x,y, and length of the snake in an int [] array
+        }return tailAndLength;
     }
+
+    public static int FindNeighbors(int i, int j, boolean[][] board){   //method to find neighbors of the current snake piece
+
+    }
+
 
     public int[] findTailRecursive(){
 
@@ -41,7 +58,8 @@ public class SnakeGame {
     }
 
     private void resetCounters(){               //resets exhaustiveChecks and recursiveChecks counter
-
+        this.exhaustiveChecks = 0;
+        this.recursiveChecks = 0;
     }
 
     //Getters////////////////////////////////////////////////////////////////////////////////////////
@@ -52,5 +70,4 @@ public class SnakeGame {
     private static int getExhaustiveChecks(){   //gets the current state of exhaustiveChecks counter
         return exhaustiveChecks;
     }
-    //test
 }
